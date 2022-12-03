b76c8abf1bfe24e337c78d5e5d35853f60addca5
William Le <115980802+WilliamAtSebratec@users.noreply.github.com>
2022-12-03 14:07:13 +0100

    finish this bransch task/ tested-complete
    Not the cleanest code, a lot of repeat step but it does the job at the moment

--- javashop-main/src/main/java/se/systementor/supershoppen1/shop/controller/AdminController.java
+++ javashop-main/src/main/java/se/systementor/supershoppen1/shop/controller/AdminController.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import se.systementor.supershoppen1.shop.services.CategoryService;
 import se.systementor.supershoppen1.shop.services.ProductService;
 
 @Controller
 public class AdminController {
     private  ProductService productService;
+    private CategoryService categoryService;
     @Autowired
-    public AdminController(ProductService productService) {
+    public AdminController(ProductService productService,CategoryService categoryService ) {
         this.productService = productService;
+        this.categoryService =categoryService;
     }    
 
     @GetMapping(path="/admin/products")
@@ -24,7 +27,30 @@ public class AdminController {
         return "admin/products";
     }
 
-  
+    @GetMapping(path="/admin/categories")
+    String showAdminCategories(Model model)
+    {
+        model.addAttribute("category1", categoryService.getName(1));
+        model.addAttribute("beverages", productService.findAllProductsByCategoryId(1));
+        model.addAttribute("category2", categoryService.getName(2));
+        model.addAttribute("condiments", productService.findAllProductsByCategoryId(2));
+        model.addAttribute("category3", categoryService.getName(3));
+        model.addAttribute("confections", productService.findAllProductsByCategoryId(3));
+        model.addAttribute("category4", categoryService.getName(4));
+        model.addAttribute("dairyproducts", productService.findAllProductsByCategoryId(4));
+        model.addAttribute("category5", categoryService.getName(5));
+        model.addAttribute("grainscereals", productService.findAllProductsByCategoryId(5));
+        model.addAttribute("category6", categoryService.getName(6));
+        model.addAttribute("meatpoultry", productService.findAllProductsByCategoryId(6));
+        model.addAttribute("category7", categoryService.getName(7));
+        model.addAttribute("produce", productService.findAllProductsByCategoryId(7));
+        model.addAttribute("category8", categoryService.getName(8));
+        model.addAttribute("seafood", productService.findAllProductsByCategoryId(8));
+        return "admin/categories";
+    }
+
+
+
 
 
 }
--- javashop-main/src/main/java/se/systementor/supershoppen1/shop/model/ProductRepository.java
+++ javashop-main/src/main/java/se/systementor/supershoppen1/shop/model/ProductRepository.java
@@ -2,6 +2,11 @@ package se.systementor.supershoppen1.shop.model;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+
+    List<Product> findProductByCategoryId(Integer id);
     
 }
