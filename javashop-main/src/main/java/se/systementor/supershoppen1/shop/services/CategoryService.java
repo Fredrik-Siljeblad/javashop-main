0435842b69b661c747ecf7a9f860a6b17fc1a744
Damir1975 <91055438+Damir1975@users.noreply.github.com>
2022-12-05 09:56:13 +0100

    category- and adminController updated

--- javashop-main/src/main/java/se/systementor/supershoppen1/shop/services/CategoryService.java
+++ javashop-main/src/main/java/se/systementor/supershoppen1/shop/services/CategoryService.java
@@ -2,10 +2,7 @@ package se.systementor.supershoppen1.shop.services;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
-
 import se.systementor.supershoppen1.shop.model.Category;
 import se.systementor.supershoppen1.shop.model.CategoryRepository;
 
