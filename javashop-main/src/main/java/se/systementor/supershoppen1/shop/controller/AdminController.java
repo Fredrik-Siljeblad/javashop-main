afdc6aed3e21b4b637b160058c594a8dc6747e0d
Damir1975 <91055438+Damir1975@users.noreply.github.com>
2022-12-05 09:41:57 +0100

    category- and adminController updatet

--- javashop-main/src/main/java/se/systementor/supershoppen1/shop/controller/AdminController.java
+++ javashop-main/src/main/java/se/systementor/supershoppen1/shop/controller/AdminController.java
@@ -1,21 +1,25 @@
 package se.systementor.supershoppen1.shop.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import se.systementor.supershoppen1.shop.model.Category;
+import se.systementor.supershoppen1.shop.services.CategoryService;
 import se.systementor.supershoppen1.shop.services.ProductService;
+
 
 @Controller
 public class AdminController {
     private  ProductService productService;
+    private CategoryService categoryService;
     @Autowired
-    public AdminController(ProductService productService) {
+    public AdminController(ProductService productService, CategoryService categoryService) {
         this.productService = productService;
-    }    
+        this.categoryService = categoryService;
+    }
 
     @GetMapping(path="/admin/products")
     String empty(Model model)
@@ -24,7 +28,21 @@ public class AdminController {
         return "admin/products";
     }
 
-  
+    @GetMapping(path="/admin/category")
+    String emptyy(Model model)
+    {
+        model.addAttribute("category", categoryService.getAll());
+        return "admin/category";
+    }
 
+    @PutMapping(path="/admin/category/create{name}{description}")
+    public Category createNewCategory(@PathVariable("name") String name, @PathVariable("description") String description) {
+        return categoryService.addCategory(name, description);
+    }
+
+    @PutMapping (path="/admin/category/edit{id}{name}{description}")
+    public Category editCategory(@PathVariable("id") Integer id, @PathVariable("name") String name, @PathVariable("description") String description) {
+        return categoryService.editCategory(id, name, description);
+    }
 
 }
--- javashop-main/src/main/java/se/systementor/supershoppen1/shop/controller/CategoryController.java
+++ javashop-main/src/main/java/se/systementor/supershoppen1/shop/controller/CategoryController.java
@@ -1,44 +1,37 @@
 package se.systementor.supershoppen1.shop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import se.systementor.supershoppen1.shop.model.Category;
 import se.systementor.supershoppen1.shop.services.CategoryService;
 
-import java.util.List;
 
 @Controller
-@RequestMapping("/category")
 public class CategoryController {
 
     private CategoryService categoryService;
 
+    @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    @GetMapping()
-    public List<Category> getAllCategories() {
-        return categoryService.getAll();
-    }
+    @GetMapping(path="/category")
+    String empty(Model model)
+    {
+        model.addAttribute("category", categoryService.getAll());
+        return "/category";    }
 
-    @GetMapping("/{id}")
+
+    @GetMapping(path="/category/{id}")
     public Category getCategoryById(@PathVariable("id") Integer id) {
         return categoryService.get(id);
     }
 
-    @PutMapping ("/create{name}{description}")
-    public Category createNewCategory(@PathVariable("name") String name, @PathVariable("description") String description) {
-        return categoryService.addCategory(name, description);
-    }
 
-    @PutMapping ("/edit{id}{name}{description}")
-    public Category editCategory(@PathVariable("id") Integer id, @PathVariable("name") String name, @PathVariable("description") String description) {
-        return categoryService.editCategory(id, name, description);
-    }
 
 
 
