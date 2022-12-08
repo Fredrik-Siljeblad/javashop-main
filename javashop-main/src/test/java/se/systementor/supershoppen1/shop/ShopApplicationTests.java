e9d17f73477395d690cb4040ca829653d7541973
Fredrik-Siljeblad <fredrik.siljeblad@gmail.com>
2022-12-08 10:33:42 +0100

    Testar review. Bara en kommentar

--- javashop-main/src/test/java/se/systementor/supershoppen1/shop/ShopApplicationTests.java
+++ javashop-main/src/test/java/se/systementor/supershoppen1/shop/ShopApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShopApplicationTests {
 
+	// Fredrik skriver en kommentar som han ber Lukas review-a
+
 	@Test
 	void contextLoads() {
 	}
