71df52c6d4ff93ba00b6072324b5527a6e3fa617
LuLukas97 <lukas.lundblad@live.se>
2022-12-21 12:34:44 +0100

    working test mail with mailtrap

--- javashop-main/src/main/java/se/systementor/supershoppen1/shop/services/EMailService/EmailServiceImp.java
+++ javashop-main/src/main/java/se/systementor/supershoppen1/shop/services/EMailService/EmailServiceImp.java
@@ -2,16 +2,34 @@
 package se.systementor.supershoppen1.shop.services.EMailService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 import se.systementor.supershoppen1.shop.model.Email;
 
+import java.util.Properties;
+
 @Service
 public class EmailServiceImp {
-        @Autowired
-        private JavaMailSender emailSender;
 
+        public JavaMailSender getJavaMailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("smtp.ethereal.email");
+            mailSender.setPort(587);
+
+            mailSender.setUsername("assunta.kihn15@ethereal.email");
+            mailSender.setPassword("nxXrHM8HUduU9XaxWv");
+
+            Properties props = mailSender.getJavaMailProperties();
+            props.put("mail.transport.protocol", "smtp");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.debug", "true");
+
+            return mailSender;
+        }
 
         public void sendSimpleMessage(Email mailObj) {
 
@@ -21,10 +39,10 @@ public class EmailServiceImp {
             message.setSubject(mailObj.getEmailSubject());
             message.setText(mailObj.getEmailContent());
             message.setReplyTo(mailObj.getEmailName());
-            emailSender.send(message);
-            System.out.println("email sent ? ");
-
+            getJavaMailSender().send(message);
         }
+
+
 
 }
 
